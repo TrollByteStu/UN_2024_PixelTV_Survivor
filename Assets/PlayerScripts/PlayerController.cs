1546e2479ff782c024e4d2cc35c899afc1054f5a
infernerner <101186811+infernerner@users.noreply.github.com>
2024-09-25 17:58:43 +0200

    added moving background ( broken when negative)

---
+++ Assets/PlayerScripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public SpriteRenderer BackgroundSprite;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private Vector2 MoveDirection;
+    private float MoveSpeed = 5;
+    private float MoveSpeedMultiplier = 1;
+    private Vector4 FinalMovement;
+    // moves Player 
+    void Movement()
+    {
+        FinalMovement = MoveDirection.ConvertTo<Vector3>() * MoveSpeed * MoveSpeedMultiplier * Time.deltaTime;
+
+        //BackgroundSprite.material.SetVector("_Offset",BackgroundSprite.material.GetVector("_Offset") + FinalMovement);
+        BackgroundSprite.material.SetVector("_Offset", BackgroundSprite.material.GetVector("_Offset")  -Vector4.one * Time.deltaTime);
+        //transform.position += MoveDirection.ConvertTo<Vector3>() * MoveSpeed * MoveSpeedMultiplier;
+    }
+    
+    // Event called by Player Input Component on press and release of move keybind
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveDirection = context.ReadValue<Vector2>();
+    }
+}

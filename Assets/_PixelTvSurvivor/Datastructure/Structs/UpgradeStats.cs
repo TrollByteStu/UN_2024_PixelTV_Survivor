579602a6aab4a1d7125bc1e530a5e7a3d040f293
sayronaught <sayro_naught@hotmail.com>
2024-10-04 21:15:39 +0200

    Upgrade system preparations

---
+++ Assets/_PixelTvSurvivor/Datastructure/Structs/UpgradeStats.cs
@@ -0,0 +1,16 @@
+using System;
+using Unity.VisualScripting;
+
+[Serializable]
+public struct UpgradeStats
+{
+    public float MaxHealthIncrease;
+    public float HealthModifierIncrease;
+    public float RecoveryIncrease;
+    public float ArmorIncrease;
+    public float MoveSpeedIncrease;
+    public float MoveSpeedModifierIncrease;
+    public float DamageModifierIncrease;
+    public float CooldownModifierIncrease;
+    public float XpModifierIncrease;
+}
---
+++ Assets/_PixelTvSurvivor/Datastructure/UpgradeData/UpgradeScriptable.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewUpgrade", menuName = "ScriptableObjects/Upgrade", order = 1)]
+public class UpgradeScriptable : ScriptableObject
+{
+    [Tooltip("The name of this Upgrade")]
+    public string UpgradeName;
+
+    [Tooltip("The image of this Upgrade")]
+    public Sprite upgradeSprite;
+
+    [Tooltip("The Level requires for this upgrade")]
+    public int levelRequired = 0;
+
+    [Tooltip("Any weapon required for this upgrade?")]
+    public WeaponStats requiedWeapon;
+
+    [Tooltip("A list of this upgrade Stats")]
+    public UpgradeStats Stats;
+
+    [Tooltip("A weapon grated for this upgrade")]
+    public WeaponStats givenWeapon;
+
+    [Tooltip("Any weapon removed by this upgrade")]
+    public WeaponStats removedWeapon;
+}

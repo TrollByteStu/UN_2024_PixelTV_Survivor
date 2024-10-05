9200da2b311ade81e158d54ca6fa5faf5688f776
sayronaught <sayro_naught@hotmail.com>
2024-10-05 07:35:09 +0200

    More UI Prepwork

--- Assets/_PixelTvSurvivor/Scripts/UI/HUD/UI_HUD.cs
+++ Assets/_PixelTvSurvivor/Scripts/UI/HUD/UI_HUD.cs
@@ -6,13 +6,38 @@ using TMPro;
 
 public class UI_HUD : MonoBehaviour
 {
+    [Header("Sections")]
+    public RectTransform Section_HUD;
+    public RectTransform Section_Looser;
+    public RectTransform Section_Upgrade;
+
+    [Space(10)]
+
+    [Header("Health Settings")]
     public RectTransform HealthSlider;
     public TMP_Text HealthText;
 
+    [Header("XP Settings")]
     public RectTransform XpSlider;
     public TMP_Text XpText;
 
+    // Make it a singleton
+    public static UI_HUD Instance { get; private set; }
+
     private PlayerStats PlayerStatRef;
+
+    private void Awake()
+    {
+        // Make it a singleton, and prevent duplicates
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
 
     // Update is called once per frame
     void Update()

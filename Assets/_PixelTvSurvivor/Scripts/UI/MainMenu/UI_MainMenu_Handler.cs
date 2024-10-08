adf72b3151174ceae562a43a7118798e941b9e04
ThomasDimovski <jazzyjuniortt@gmail.com>
2024-10-08 14:25:16 +0200

    butten background and animeson

--- Assets/_PixelTvSurvivor/Scripts/UI/MainMenu/UI_MainMenu_Handler.cs
+++ Assets/_PixelTvSurvivor/Scripts/UI/MainMenu/UI_MainMenu_Handler.cs
@@ -1,20 +1,26 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UI_MainMenu_Handler : MonoBehaviour
 {
+    public GameObject NewAnimationExit;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+    public void tooltipLettuce() { 
+    NewAnimationExit.GetComponent<TextMeshProUGUI>().text = "+MP";
+     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     public void PressPlay()

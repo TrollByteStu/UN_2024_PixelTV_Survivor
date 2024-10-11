a4797ff1b07e6ca3d703280172eb2e52701d6fbd
infernerner <101186811+infernerner@users.noreply.github.com>
2024-10-11 12:22:19 +0200

    started work on orbit weapon

---
+++ Assets/_PixelTvSurvivor/Datastructure/Attacks/WeaponOrbit.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WeaponOrbit : WeaponBase
+{
+    private List<GameObject> Satellites = new List<GameObject>();
+    public GameObject Satellite;
+    public List<WeaponStats> LevelStats;
+    [Serializable]
+    public struct WeaponStats
+    {
+        public float DistanceFromPlayer;
+        public float RotationSpeed;
+        public float AttackDamage;
+        public float SatelliteQuantity;
+    }
+
+    public override void Attack(int level, Vector3 playerPosition, Vector3 direction, PlayerStats playerStats)
+    {
+        for (int i = 0; i < LevelStats[level].SatelliteQuantity; i++)
+        {
+            if (Satellites[i] == null)
+                Satellites[i] = Instantiate(Satellite);
+
+        }
+    }
+    public override float GetAttackSpeed(int level)
+    {
+        return 0;
+    }
+
+}

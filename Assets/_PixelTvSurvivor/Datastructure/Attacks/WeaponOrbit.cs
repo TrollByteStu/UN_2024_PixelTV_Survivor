838a031b915e4e5958429929b15117e45af77f6b
infernerner <101186811+infernerner@users.noreply.github.com>
2024-10-12 15:54:54 +0200

    orbit layers work

--- Assets/_PixelTvSurvivor/Datastructure/Attacks/WeaponOrbit.cs
+++ Assets/_PixelTvSurvivor/Datastructure/Attacks/WeaponOrbit.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WeaponOrbit", menuName = "ScriptableObjects/WeaponTypes/Orbit", order = 1)]
 public class WeaponOrbit : WeaponBase
 {
+    private Transform SatelliteHolder;
     private List<GameObject> Satellites = new List<GameObject>();
     public GameObject Satellite;
     public List<WeaponStats> LevelStats = new List<WeaponStats> { new WeaponStats() };
@@ -32,16 +33,21 @@ public class WeaponOrbit : WeaponBase
 
     public override void Attack(int level, Vector3 playerPosition, Vector3 direction, PlayerStats playerStats)
     {
+        if (SatelliteHolder == null)
+            SatelliteHolder = new GameObject("SatelliteHolder").transform;
+
+        int QuantityPerLayer = LevelStats[level].SatelliteQuantity / LevelStats[level].LayerQuantity;
         for (int i = 0; i < LevelStats[level].SatelliteQuantity; i++)
-        {
+        { 
             if (Satellites.Count == i)
-                Satellites.Add(Instantiate(Satellite));
+                Satellites.Add(Instantiate(Satellite,SatelliteHolder));
             if (Satellites[i] == null)
-                Satellites[i] = Instantiate(Satellite);
+                Satellites[i] = Instantiate(Satellite,SatelliteHolder);
+
 
             Satellites[i].transform.position = new Vector3(
-            /*x*/    math.sin(Time.timeSinceLevelLoad * LevelStats[level].RotationSpeed + (math.PI * 2) / (LevelStats[level].SatelliteQuantity / LevelStats[level].LayerQuantity) * (i % (LevelStats[level].SatelliteQuantity / LevelStats[level].LayerQuantity))) * LevelStats[level].DistanceFromPlayer / math.floor(i / (LevelStats[level].SatelliteQuantity / LevelStats[level].LayerQuantity) +1),
-            /*y*/    math.cos(Time.timeSinceLevelLoad * LevelStats[level].RotationSpeed + (math.PI * 2) / (LevelStats[level].SatelliteQuantity / LevelStats[level].LayerQuantity) * (i % (LevelStats[level].SatelliteQuantity / LevelStats[level].LayerQuantity))) * LevelStats[level].DistanceFromPlayer / math.floor(i / (LevelStats[level].SatelliteQuantity / LevelStats[level].LayerQuantity) +1), 0) + playerPosition;
+            /*x*/    math.sin(Time.timeSinceLevelLoad * LevelStats[level].RotationSpeed + (math.PI * 2) / QuantityPerLayer * (i % QuantityPerLayer)) * (LevelStats[level].DistanceFromPlayer / LevelStats[level].LayerQuantity * math.floor((i + QuantityPerLayer) / QuantityPerLayer)),
+            /*y*/    math.cos(Time.timeSinceLevelLoad * LevelStats[level].RotationSpeed + (math.PI * 2) / QuantityPerLayer * (i % QuantityPerLayer)) * (LevelStats[level].DistanceFromPlayer / LevelStats[level].LayerQuantity * math.floor((i + QuantityPerLayer) / QuantityPerLayer)), 0) + playerPosition;
         }
         foreach (GameObject obj in Satellites.GetRange(LevelStats[level].SatelliteQuantity, Satellites.Count - LevelStats[level].SatelliteQuantity))
         {

5f2625e692ac864178343d41836d5bb423cb0dc4
Snubig1 <71340977+Snubig1@users.noreply.github.com>
2024-10-22 14:16:45 +0200

    decal spawn

--- Assets/_PixelTvSurvivor/Scripts/Decor/Main_Decor.cs
+++ Assets/_PixelTvSurvivor/Scripts/Decor/Main_Decor.cs
@@ -6,24 +6,73 @@ using UnityEngine.Tilemaps;
 public class Main_Decor : MonoBehaviour
 {
     private Tilemap myTM;
-
+    
     public TileBase[] Decortiles;
 
-
+    Vector3 playerPos;
+    Vector3Int testPos;
+    bool decorationFound;
 
     // Start is called before the first frame update
     void Start()
     {
         myTM = GetComponent<Tilemap>();
-        myTM.SetTile(new Vector3Int(5, 5, 0), Decortiles[0]);
-        myTM.SetTile(new Vector3Int(15, 15, 0), Decortiles[0]);
-        int random = Random.Range(0, Decortiles.Length - 1);
-        Vector3 playerPos = GameController.Instance.PlayerReference.transform.position;
+        playerPos = GameController.Instance.PlayerReference.transform.position;
+
+
+        for (int i = 1; i < 100; i++)
+        {
+            playerPos = GameController.Instance.PlayerReference.transform.position;
+            int random = Random.Range(0, 360);
+            testPos = Vector3Int.RoundToInt(playerPos + new Vector3(Mathf.Cos(random) * (int)(i*0.2), Mathf.Sin(random) * (int)(i * 0.2)));
+
+            decorationFound = false;
+            for (int x = -5; x < 5; x++)
+            {
+                for (int y = -5; y < 5; y++)
+                {
+                    if (myTM.GetTile(testPos + new Vector3Int(x, y, 0)) != null)
+                    {
+                        decorationFound = true;
+                    }
+                }
+            }
+
+            if (!decorationFound)
+            {
+                myTM.SetTile(testPos, Decortiles[0]);
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerPos = GameController.Instance.PlayerReference.transform.position;
+        int random = Random.Range(0, 360);
+        testPos = Vector3Int.RoundToInt(playerPos + new Vector3(Mathf.Cos(random) * 20, Mathf.Sin(random) * 20));
+
+        decorationFound = false;
+        for (int x = -5; x < 5; x++)
+        {
+            for (int y = -5; y < 5; y++)
+            {
+                if ( myTM.GetTile(testPos+ new Vector3Int(x,y,0)) != null)
+                {
+                    decorationFound = true;
+                }
+            }
+        }
+
+        if ( !decorationFound )
+        {
+            myTM.SetTile(testPos, Decortiles[0]);
+        }
         
+    }
+    private Vector3Int testpos(Vector3Int testPost, int size)
+    {
+        return new Vector3Int(testPos.x + size, testPos.y + size, 0);
     }
 }

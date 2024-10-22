9c3b7bfeaab56cbf32db594cc6d91ea17f7672e8
ThomasDimovski <jazzyjuniortt@gmail.com>
2024-10-22 14:19:33 +0200

    code for the slot

--- Assets/_PixelTvSurvivor/Scripts/UI/SlotMachine/UI_Slotmachine.cs
+++ Assets/_PixelTvSurvivor/Scripts/UI/SlotMachine/UI_Slotmachine.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Runtime.CompilerServices;
 
-public class UI_Slotmachine : MonoBehaviour
+public class UI_Slotmachine : MonoBehaviour 
+
+public class event Action Levelup = delegate { };
 {
+    [SerializeField]
+    private Text prizeText;
+
+    [SerializeField]
+    private slot spine[] slost spines;
+
+    [SerializeField]
+    private Trasnform handle,
+
+private bool resulstChecked = false;
+
     // Start is called before the first frame update
-    void Start()
+    void Start() {
+    íf (!slot spines[0].slot spineStopped || !slot spines[1].slot spineStopped || !slot spines[2].slot spineStopped)
+
     {
         
     }

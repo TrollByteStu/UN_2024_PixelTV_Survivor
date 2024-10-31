3b83a1f9ed6c9c84dc3280ab549e50019836f05f
sayronaught <sayro_naught@hotmail.com>
2024-11-01 00:10:35 +0100

    Animated Santa maincharacter walk/idle

--- Assets/_PixelTvSurvivor/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/_PixelTvSurvivor/Scripts/PlayerScripts/PlayerController.cs
@@ -9,9 +9,15 @@ using System;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Stats")]
     public PlayerStats Stats = new("Player",100,100,0.1f,0,10);
-    
     public WeaponStats[] WeaponsArray;
+
+    [Header("Santa Sprite Layered")]
+    public GameObject Santa_Body_Idle;
+    public GameObject Santa_Body_Walk;
+    public GameObject Santa_Gun_Idle;
+    public GameObject Santa_Gun_Walk;
 
     private void Awake()
     {
@@ -124,7 +130,7 @@ public class PlayerController : MonoBehaviour
     {
         MoveDirection = context.ReadValue<Vector2>().normalized;
         if (MoveDirection != Vector2.zero)
-        {
+        { // is moving
             AimDirection = MoveDirection;
             foreach (WeaponStats weaponStats in WeaponsArray)
             {
@@ -136,6 +142,16 @@ public class PlayerController : MonoBehaviour
                 transform.localScale = new Vector3(-.5f, .5f, 1);
             else
                 transform.localScale = new Vector3(.5f, .5f, 1);
+
+            Santa_Body_Idle.SetActive(false);
+            Santa_Body_Walk.SetActive(true);
+            Santa_Gun_Idle.SetActive(false);
+            Santa_Gun_Walk.SetActive(true);
+        } else { // not moving
+            Santa_Body_Idle.SetActive(true);
+            Santa_Body_Walk.SetActive(false);
+            Santa_Gun_Idle.SetActive(true);
+            Santa_Gun_Walk.SetActive(false);
         }
     }
 

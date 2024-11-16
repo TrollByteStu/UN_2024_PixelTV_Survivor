3c7ad2bdf84d0ed119569b0e7a6501288d93b179
sayronaught <sayro_naught@hotmail.com>
2024-11-16 01:35:52 +0100

    New decor from from Graphics team

--- Assets/_PixelTvSurvivor/Scripts/Decor/Main_Decor.cs
+++ Assets/_PixelTvSurvivor/Scripts/Decor/Main_Decor.cs
@@ -12,6 +12,8 @@ public class Main_Decor : MonoBehaviour
     Vector3 playerPos;
     Vector3Int testPos;
     bool decorationFound;
+    int random;
+    int areasize;
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +25,14 @@ public class Main_Decor : MonoBehaviour
         for (int i = 1; i < 100; i++)
         {
             playerPos = GameController.Instance.PlayerReference.transform.position;
-            int random = Random.Range(0, 360);
+            random = Random.Range(0, 360);
             testPos = Vector3Int.RoundToInt(playerPos + new Vector3(Mathf.Cos(random) * (int)(i*0.2), Mathf.Sin(random) * (int)(i * 0.2)));
+            areasize = Random.Range(5, 15);
 
             decorationFound = false;
-            for (int x = -5; x < 5; x++)
+            for (int x = -areasize; x < areasize; x++)
             {
-                for (int y = -5; y < 5; y++)
+                for (int y = -areasize; y < areasize; y++)
                 {
                     if (myTM.GetTile(testPos + new Vector3Int(x, y, 0)) != null)
                     {
@@ -52,11 +55,12 @@ public class Main_Decor : MonoBehaviour
         playerPos = GameController.Instance.PlayerReference.transform.position;
         int random = Random.Range(0, 360);
         testPos = Vector3Int.RoundToInt(playerPos + new Vector3(Mathf.Cos(random) * 20, Mathf.Sin(random) * 20));
+        areasize = Random.Range(5, 15);
 
         decorationFound = false;
-        for (int x = -5; x < 5; x++)
+        for (int x = -areasize; x < areasize; x++)
         {
-            for (int y = -5; y < 5; y++)
+            for (int y = -areasize; y < areasize; y++)
             {
                 if ( myTM.GetTile(testPos+ new Vector3Int(x,y,0)) != null)
                 {

41272189e325e1be20e0ee4c940c7efe1636b824
Erik Pedersen <sayro_naught@hotmail.com>
2024-11-18 13:03:59 +0100

    steamroller sounds

--- Assets/_PixelTvSurvivor/Scripts/Enemies/Steamroller.cs
+++ Assets/_PixelTvSurvivor/Scripts/Enemies/Steamroller.cs
@@ -6,17 +6,21 @@ public class Steamroller : MonoBehaviour
     public Vector3 direction;
 
     private PlayerController playerRef;
+    private AudioSource myAS;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRef = GameController.Instance.PlayerReference;
+        myAS = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += direction * Time.deltaTime;
+
+        myAS.volume = (15 - Vector3.Distance(playerRef.transform.position, transform.position))*0.1f;
 
         // kill enemy if to far from player
         if (Vector3.Distance(playerRef.transform.position, transform.position) > 30)
